4b45360dbe84db30d9fd6eb0482eba242ebdae8d
Sergio-Prada-963 <alejanprada140705@gmail.com>
2023-10-06 15:18:41 -0500

    build: :zap: funshionnn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import NavBar from "./components/nav_bar/Nav";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Login from "./components/login/Login";
 
 function App() {
@@ -9,12 +9,13 @@ function App() {
         <Fragment>
              <Router>
             <Switch>
-                <Route path="/productos">
-                    <NavBar/>
-                </Route>
-                <Route path="/">
-                    <Login/>
-                </Route>
+                
+                    <Route path={"/productos"}>
+                    {document.cookie ? <NavBar/> : <Redirect  to={"/"}/> }
+                    </Route>
+                    <Route path="/">
+                    {document.cookie ? <Redirect  to={"/productos"} /> :  <Login/>  }
+                    </Route>
             </Switch>
             </Router>
         </Fragment>
--- frontend/src/components/nav_bar/Nav.jsx
+++ frontend/src/components/nav_bar/Nav.jsx
@@ -1,12 +1,17 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Productos from "../productos/Productos";
 import Conceccionarios from "../conceccionarios/Conceccionarios";
 import Proveedores from "../proveedores/Proveedores";
 import Empleados from "../empleados/Empleados";
 import Ventas from "../ventas/Ventas";
+import Login from "../login/Login";
 
 const NavBar = () => {
+    const logout = ()=>{
+        document.cookie = "tokenX=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+        window.location.reload();
+    }
     return (
         <Fragment>
             <Router>
@@ -39,7 +44,7 @@ const NavBar = () => {
                         </ul>
                     </div>
                     <div className="salir">
-                        <Link to="/login">
+                        <Link to="/" onClick={()=>logout()}>
 							<span>Logout</span><svg width="30" height="30" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_10_246)"><path d="M23.3333 13.3333V9.99999C23.3333 9.11593 22.9821 8.26809 22.357 7.64297C21.7319 7.01785 20.8841 6.66666 20 6.66666H8.33333C7.44928 6.66666 6.60143 7.01785 5.97631 7.64297C5.35119 8.26809 5 9.11593 5 9.99999V30C5 30.884 5.35119 31.7319 5.97631 32.357C6.60143 32.9821 7.44928 33.3333 8.33333 33.3333H20C20.8841 33.3333 21.7319 32.9821 22.357 32.357C22.9821 31.7319 23.3333 30.884 23.3333 30V26.6667" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M15 20H35L30 15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M30 25L35 20" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></g><defs><clipPath id="clip0_10_246"><rect width="40" height="40" fill="white"/></clipPath></defs></svg>
 						</Link>
                     </div>
@@ -61,8 +66,8 @@ const NavBar = () => {
                     <Route path="/ventas">
                         <Ventas />
                     </Route>
-                    <Route path="/login">
-                        <h1>login prro</h1>
+                    <Route path="/">
+                        <Login />
                     </Route>
                 </Switch>
             </Router>
